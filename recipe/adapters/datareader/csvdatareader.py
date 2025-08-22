9c757cfa64e957561397a16d7e36ed83b4a5a616
ziqfan <mjam705@aucklanduni.ac.nz>
2025-08-22 14:08:06 +1200

    Hazziq modify csvdatareader and nutrition.py following sid's

--- recipe/adapters/datareader/csvdatareader.py
+++ recipe/adapters/datareader/csvdatareader.py
@@ -1,11 +1,62 @@
-import os
+
 import csv
 from recipe.domainmodel.author import Author
 from recipe.domainmodel.category import Category
 from recipe.domainmodel.nutrition import Nutrition
 from recipe.domainmodel.recipe import Recipe
-
+from datetime import datetime
 
 class CSVDataReader:
-    # TODO: Complete the implementation of the CSVDataReader class.
-    pass
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.nutriID_count = 1
+
+    def read_recipes(self):
+        recipes = []
+        with open(self.file_path, newline='', encoding="utf-8") as csvfile:
+            reader = csv.reader(csvfile)
+            next(reader)  # skip header
+            for row in reader:
+                # nutrition id - since it don't have id, I put incremental id instead
+                nutriID = self.nutriID_count
+                self.nutriID_count += 1
+
+                # Recipe: id, name, description, ingredients, instructions, cookingTime
+                id = int(row[0].strip())
+                name = row[1].strip()
+                description = row[8].strip()
+                ingredients = [row[12].strip()]
+                instructions = [row[24].strip()]
+                cookingTime = int(row[4].strip())
+                preparationTime = int(row[5].strip())
+                datePublished = datetime(row[7].strip())
+                imagesLink = [row[9].strip()]
+                ingrQuantities = [row[11].strip()]
+                servings = row[22].strip()
+                recipeYield = row[23].strip()
+                rating = int(input("Enter your rating: "))
+
+                # Author: id, name
+                author = Author(int(row[2]), str(row[3]), [])
+                # declare category
+                category = Category(row[10].strip(), [])
+                # declare: cal, fat, saturated fate, cholesterol, sodium, carbohydrate, fiber, sugar, protein
+                nutrition = Nutrition(nutriID, float(row[13]), float(row[14]), float(row[15]), float(row[16]), float(row[17]),
+                                      float(row[18]), float(row[19]), float(row[20]), float(row[21]))
+                recipe = Recipe(id, name, author, cookingTime, preparationTime, datePublished, description, imagesLink, category,
+                                ingrQuantities, ingredients, rating, nutrition, servings, recipeYield, instructions)
+
+                # list of recipes: category
+                category.add_recipe(recipe)
+                # list of recipes: author
+                author.add_recipe(recipe)
+                recipes.append(recipe)
+        return recipes
+
+# main
+if __name__ == "__main__":
+    reader = CSVDataReader("recipe/adapters/data/recipes.csv")
+    recipes = reader.read_recipes()
+    for r in recipes:
+        print(f"{r.name} by {r.author.name} [{r.category.name}] -> {r.nutrition.calories} cal")
+
--- recipe/domainmodel/nutrition.py
+++ recipe/domainmodel/nutrition.py
@@ -1,19 +1,39 @@
 class Nutrition:
 # TODO: Complete the implementation of the Nutrition class.
-    def __init__(self, id: int, calories: int = None, fat: int = None,
-                 saturatedfat: int = None, cholesterol:int = None,
-                 sodium: int = None, carbohydrates: int = None,
-                 fiber: int = None, sugar:int = None, protein:int = None) -> None:
+    def __init__(self, id: int, calories: float = None, fat: float = None,
+                 saturatedfat: float = None, cholesterol:float = None,
+                 sodium: float = None, carbohydrates: float = None,
+                 fiber: float = None, sugar:float = None, protein:float = None) -> None:
         self.__id = id
-        self.__calories = calories
-        self.__fat = fat
-        self.__saturatedfat = saturatedfat
-        self.__cholesterol = cholesterol
-        self.__sodium = sodium
-        self.__carbohydrates = carbohydrates
-        self.__fiber = fiber
-        self.__sugar = sugar
-        self.__protein = protein
+        #self.__calories = calories
+        #self.__fat = fat
+        #self.__saturatedfat = saturatedfat
+        #self.__cholesterol = cholesterol
+        #self.__sodium = sodium
+        #self.__carbohydrates = carbohydrates
+        #self.__fiber = fiber
+        #self.__sugar = sugar
+        #self.__protein = protein
+        self.__nutris = {"calories": calories, "fat": fat, "saturatedfat": saturatedfat, "cholesterol": cholesterol, "sodium": sodium, "carbohydrates": carbohydrates, "fiber": fiber , "sugar": sugar, "protein": protein}
+
+        def __repr__(self):
+            return str(self.__nutris)
+
+        def __eq__(self, other, nutri):
+            if isinstance(other, Nutrition):
+                return self.__nutris[nutri] == other.__nutris[nutri]
+            else:
+                return False
+
+        def __lt__(self, other, nutri):
+            if isinstance(other, Nutrition):
+                return self.__nutris[nutri] < other.__nutris[nutri]
+            else:
+                return False
+
+        def __hash__(self) -> int:
+            return hash(self.id)
+
 
 
         @property
@@ -21,28 +41,28 @@ class Nutrition:
             return self.__id
         @property
         def calories(self) -> int:
-            return self.__calories
+            return self.__nutirs["calories"]
         @property
         def fat(self) -> int:
-            return self.__fat
+            return self.__nutris["fat"]
         @property
         def saturatedfat(self) -> int:
-            return self.__saturatedfat
+            return self.__nutris["saturatedfat"]
         @property
         def cholesterol(self) -> int:
-            return self.__cholesterol
+            return self.__nutris["cholesterol"]
         @property
         def sodium(self) -> int:
-            return self.__sodium
+            return self.__nutris["sodium"]
         @property
         def carbohydrates(self) -> int:
-            return self.__carbohydrates
+            return self.__nutris["carbohydrates"]
         @property
         def fiber(self) -> int:
-            return self.__fiber
+            return self.__nutris["fiber"]
         @property
         def sugar(self) -> int:
-            return self.__sugar
+            return self.__nutris["sugar"]
         @property
         def protein(self) -> int:
-            return self.__protein
+            return self.__nutris["protein"]
